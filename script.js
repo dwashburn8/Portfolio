21e53b22f1e3d763e5b62c397c885b0777f0947d
Dallas Washburn <soldbydw@gmail.com>
2020-04-22 11:02:48 -0700

    added html and css

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// $(document).ready(function(){
+//     var key = "16163467-c85d6318b5a5c94f6bdcb2597"
+//     var search = "stars-long-exposure"
+// var queryUrl = "https://pixabay.com/api/videos/?key=" + key + "&q=" + search;
+
+// $.ajax({
+//     url: queryUrl,
+//     method: "GET"
+// }).then(function(response){
+//     var selectedVid = response.hits[0].videos.large.url
+//     console.log(selectedVid);
+//     // $("#video").attr("src", selectedVid)
+// })
+
+
+// })
+
+
